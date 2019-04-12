37cd32f758322c509e5729dc05fe5609c30f3df6
DIGIREN <matthewmcdonoughd@gmail.com>
2019-04-12 18:35:05 -0400

    Removing unused imports from layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql, Link } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 import { Header } from '../Header';
 import { Container } from '../Container';
 import { ResetStyle } from '../Reset';
-import { NAV_ITEMS, closeMenu } from '../../constants';
 
 // This needs changed the second this is more than a one page site. Need to find a better way to inject global styles. Better yet, TODO: make a seperate header styled component. Allow it to accept image prop.
 
